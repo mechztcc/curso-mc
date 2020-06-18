2f788b8b5e4ad7389146bb89818c81531ac7e4f1
Alberto <dojathan@gmail.com>
2020-06-18 07:48:08 -0300

    creating repository and service to category

---
+++ src/main/java/com/alberto/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.alberto.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alberto.cursomc.domain.Categoria;
+import com.alberto.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);
+	}
+}
