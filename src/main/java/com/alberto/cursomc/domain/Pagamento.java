1b0fc85b9a5511b313fcadb83563fc7a1453ee3e
Alberto <dojathan@gmail.com>
2020-06-22 19:53:17 -0300

    Endpoint /pedidos/{id} available

--- src/main/java/com/alberto/cursomc/domain/Pagamento.java
+++ src/main/java/com/alberto/cursomc/domain/Pagamento.java
@@ -11,6 +11,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.alberto.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -24,7 +25,7 @@ public abstract class Pagamento implements Serializable{
 	
 	private Integer estado;
 	
-	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
