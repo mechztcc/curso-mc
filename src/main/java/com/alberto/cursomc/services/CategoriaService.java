6fb754a0d932f0ba2d1d88e0aa9a675a25980d8c
Alberto <dojathan@gmail.com>
2020-06-26 07:56:22 -0300

    Delete method to categorias created

--- src/main/java/com/alberto/cursomc/services/CategoriaService.java
+++ src/main/java/com/alberto/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.alberto.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.alberto.cursomc.domain.Categoria;
 import com.alberto.cursomc.repositories.CategoriaRepository;
+import com.alberto.cursomc.services.exceptions.DataIntegrityException;
 import com.alberto.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -36,5 +38,17 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+		repo.deleteById(id);
+		}
+		catch( DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria "
+					+ "que possui produtos!");
+		}
 	}
 }
