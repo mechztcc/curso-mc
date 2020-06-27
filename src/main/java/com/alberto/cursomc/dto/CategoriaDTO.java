3c5084bc6b3b4d63171d347e76117b511a4bd614
Alberto <dojathan@gmail.com>
2020-06-27 09:49:57 -0300

    Using patten DTO to list categorias

---
+++ src/main/java/com/alberto/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,37 @@
+package com.alberto.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.alberto.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	public Integer id;
+	public String nome;
+	
+	public CategoriaDTO() {}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+	
+}
--- src/main/java/com/alberto/cursomc/services/CategoriaService.java
+++ src/main/java/com/alberto/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.alberto.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,5 +51,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria "
 					+ "que possui produtos!");
 		}
+	}
+	
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
