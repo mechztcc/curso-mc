4706514819597ba2670c27a544f5c97580e3dc09
Alberto <dojathan@gmail.com>
2020-07-18 09:27:32 -0300

    Validation to non-repeat email

--- src/main/java/com/alberto/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/alberto/cursomc/repositories/ClienteRepository.java
@@ -1,11 +1,16 @@
 package com.alberto.cursomc.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alberto.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
