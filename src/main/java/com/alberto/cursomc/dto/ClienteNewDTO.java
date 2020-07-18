1d3df2dc8b0edf431cd1a32b5d0e63ef90c08752
Alberto <dojathan@gmail.com>
2020-07-18 08:41:07 -0300

    custumized valadation for CPF or CNPJ in POST to Cliente

--- src/main/java/com/alberto/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/alberto/cursomc/dto/ClienteNewDTO.java
@@ -2,27 +2,57 @@ package com.alberto.cursomc.dto;
 
 import java.io.Serializable;
 
-public class ClienteNewDTO implements Serializable{
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.alberto.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
+public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
 	private String email;
+
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
+
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
+
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
+
 	private String complemento;
+
 	private String bairro;
+
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
+
 	private String telefone2;
-	private String telefone3;
 	
+	private String telefone3;
+
 	private Integer cidadeId;
 	
-	public ClienteNewDTO() {}
+	public ClienteNewDTO() {
+	}
 
 	public String getNome() {
 		return nome;
@@ -46,6 +76,14 @@ public class ClienteNewDTO implements Serializable{
 
 	public void setCpfOuCnpj(String cpfOuCnpj) {
 		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+	public Integer getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
 	}
 
 	public String getLogradouro() {
@@ -119,14 +157,13 @@ public class ClienteNewDTO implements Serializable{
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
-
-	public Integer getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(Integer tipo) {
-		this.tipo = tipo;
+	
+	public String getSenha() {
+		return senha;
 	}
 	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 	
 }
